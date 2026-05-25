e6374dd83d040f554f8da17f56d1f7d658e4228e
Aayush <aayush01v@gmail.com>
2026-05-25 23:41:39 +0530

    Delete vectorize.py

--- vectorize.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Cross-platform wrapper:
-    python vectorize.py input.png output.svg --preset portrait_clean
-"""
-from vector_photo_tracer.cli import main
-
-if __name__ == "__main__":
-    main()
